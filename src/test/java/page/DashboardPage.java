80a248a8264f7621cf3c7764d60cc6aaf77a4c50
Elena Zimnickaya <nazarova.leno4ka@yandex.ru>
2023-06-22 23:42:31 +0400

    Get text

--- src/test/java/page/DashboardPage.java
+++ src/test/java/page/DashboardPage.java
@@ -9,6 +9,7 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
+
 public class DashboardPage {
     private SelenideElement heading = $("[data-test-id=dashboard]");
     private ElementsCollection card = $$(".list__item");
